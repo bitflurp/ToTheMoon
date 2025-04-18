c762c62f1737aa9186e44b73979eefa66df5eefd
bitflurp <mandy.guilty@gmail.com>
2025-04-18 11:44:18 +0200

    Changes for the Build version

--- Assets/Scripts/TilemapControls.cs
+++ Assets/Scripts/TilemapControls.cs
@@ -4,7 +4,7 @@ using UnityEngine.Tilemaps;
 using TMPro;
 using System.Collections.Generic;
 using UnityEngine.Rendering.Universal;
-using TreeEditor;
+
 
 public class TilemapControls : MonoBehaviour
 {
